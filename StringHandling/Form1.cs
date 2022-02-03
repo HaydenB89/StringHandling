be91b7b68104631a01339572f371a57072d5af67
hayden brown <haydenbrowncinci@gmail.com>
2022-02-03 16:09:51 -0500

    cont.1

--- StringHandling/Form1.cs
+++ StringHandling/Form1.cs
@@ -20,11 +20,20 @@ namespace StringHandling
         private void btnParseName_Click(object sender, System.EventArgs e)
         {
             // TODO: Add code to parse name
+            
         }
 
         private void btnEditPhoneNumber_Click(object sender, System.EventArgs e)
         {
             // TODO: Add code to edit the phone number
+            string phoneNumber = txtPhoneNumber.Text.Replace("(",""); // now that you've made "phoneNumber" something else, you have to make a new variable to amke more changes
+            string digitsOnly = phoneNumber.Replace(")","");          // now that you created dO from pN you have to keep modifying dO to remove all other spc.chars
+            digitsOnly = digitsOnly.Replace(" ", "");
+            digitsOnly = digitsOnly.Replace("-","");
+            //string standardFormat = string.Empty;
+            string standardFormat = digitsOnly.Substring(0,3) 
+
+
         }
 
         // TODO: Add ToInitialCap method here
